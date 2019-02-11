b0765b95b4505500a4b61e0a535c753e8fb9c843
gawao <gawao57@gmail.com>
2019-02-11 12:20:08 +0900

    create nuxt app (TypeScript)

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  // add your custom rules here
+  rules: {}
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,81 @@
+import { Configuration } from 'webpack'
+import { Context } from '@nuxt/vue-app'
+
+import pkg from './package.json'
+
+export default {
+  mode: 'spa',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma'
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config: Configuration, ctx: Context) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        if (config.module) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue|ts)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
+          })
+        }
+      }
+    }
+  }
+}
