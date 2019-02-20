154a145d9959d256f6e43ddba7e58822098ff276
gawao <gawao57@gmail.com>
2019-02-20 10:52:37 +0900

    feat: Nuxt TypeScript watch test

---
+++ components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        あばうと
+      </div>
+      <div
+        @click="toggleMenu"
+        :class="{ 'is-active': isMenuActive }"
+        class="navbar-burger"
+        data-target="navMenu"
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+
+    <div
+      id="navMenu"
+      :class="{ 'is-active': isMenuActive }"
+      class="navbar-menu"
+    >
+      <div class="navbar-end">
+        <nuxt-link to="/" class="navbar-item">
+          トップ
+        </nuxt-link>
+        <nuxt-link to="/about" class="navbar-item">
+          あばうと
+        </nuxt-link>
+        <nuxt-link to="/contact" class="navbar-item">
+          こんたくと
+        </nuxt-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+
+@Component
+export default class Navbar extends Vue {
+  // コンポーネント内の状態 data
+  isMenuActive: boolean = false
+
+  // ライフサイクル
+  mounted(): void {
+    // eslint-disable-next-line no-console
+    console.log('mounted')
+  }
+
+  // コンポーネント内のメソッド
+  toggleMenu(): void {
+    this.isMenuActive = !this.isMenuActive
+  }
+
+  // dataの変更を監視し、変更時に呼び出されるメソッドを定義
+  @Watch('$route')
+  onRouteChange(): void {
+    this.isMenuActive = false
+  }
+}
+</script>
